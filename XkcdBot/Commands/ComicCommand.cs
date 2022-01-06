b45e015892576b5cde17ab1034f2a9302cbd3641
Trevor Reeves <reevest2@gmail.com>
2022-01-06 16:24:46 -0500

    added comic command

---
+++ XkcdBot/Commands/ComicCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using RestSharp;
+using XkcdBot.Models;
+
+namespace XkcdBot.Commands
+{
+    public class PhotoCommand : CommandBase
+    {
+        [Command("comic")]
+        public async Task HandleCommandAsync()
+        {
+            var restClient = new RestClient();
+            var request = new RestRequest("https://xkcd.com/info.0.json");
+            var response = await restClient.ExecuteAsync<ComicModel>(request);
+
+            if (response.Data == null || string.IsNullOrEmpty(response.Data.Img))
+            {
+                await ReplyAsync("No comics found.");
+            }
+            else
+            {
+                await ReplyAsync(response.Data.Img);
+            }
+        }
+    }
+}
---
+++ XkcdBot/Models/ComicModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XkcdBot.Models
+{
+    public class ComicModel
+    {
+        public string Link { get; set; }
+
+        public string Title { get; set; }
+
+        public int Num { get; set; }
+
+        public string Img { get; set; }
+    }
+}
+
+
